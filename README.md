64b9ea56a6b4818a73a4ce523973e946b9671305
cxz5309 <42149645+cxz5309@users.noreply.github.com>
2021-01-20 06:57:23 +0900

    pug Html 진행중

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Tube
 
 유튜브 클론코딩
+
+## Pages:
+
+- [ ] Home
+- [ ] Join
+- [ ] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Upload
+- [ ] Video Datail
+- [ ] Edit Video
+- [ ] Delete Video
+
+
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";   
 import cookieParser from "cookie-parser";   
 import bodyParser from "body-parser";   
+import {localsMiddleware} from "./middlewares";
 import routes from "./routes"
 import userRouter from "./routers/userRouter"
 import videoRouter from "./routers/videoRouter"
@@ -10,13 +11,15 @@ import globalRouter from "./routers/globalRouter"
 
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(helmet());
 app.use(morgan("dev"));
 app.use(morgan("combined"));
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
