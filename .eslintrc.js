2acd93308609fd4e2ee727d7de4d88b40813380e
cxz5309 <42149645+cxz5309@users.noreply.github.com>
2021-01-25 23:52:19 +0900

    login, user, video db, 오류해결중

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,4 @@
 module.exports = {
     extends: ["airbnb-base", "plugin:prettier/recommended"],
-    rules: {
-      "no-console": "off"
-    }
+    eslint:recommended
   };
--- routes.js
+++ routes.js
@@ -1,5 +1,3 @@
-import app from "./app";
-
 const HOME = "/";
 const JOIN = "/join";
 const LOGIN = "/login";
@@ -12,6 +10,7 @@ const USERS = "/users";
 const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
+const ME = "/me";
 
 //Videos
 
@@ -20,6 +19,11 @@ const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
+
+//Github
+
+const GITHUB = "/auth/github";
+const GITHUB_CALLBACK = "/auth/github/callback";
 
 const routes = {
     home: HOME,
@@ -61,7 +65,10 @@ const routes = {
       else{
         return DELETE_VIDEO;
       }
-    }
+    },
+    gitHub: GITHUB,
+    githubCallback: GITHUB_CALLBACK,
+    me:ME
 }
 
 export default routes;
